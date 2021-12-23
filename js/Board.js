3e0df9bfb851718bdc5fca8d3ef67d81335ac432
Subhendu Hazra <subhenduhazra@Subhendus-MacBook-Air.local>
2021-12-23 20:40:33 +0530

    boards rendering fixed

--- js/Board.js
+++ js/Board.js
@@ -1,11 +1,11 @@
 import { Snake } from "./Snake.js";
 import { FoodController } from "./FoodController.js";
 export class Board{
-    constructor(width=300,height=300,parent=document.getElementsByTagName('body')[0]){
+    constructor(width=30,height=30,parent=document.getElementsByTagName('body')[0]){
         this.width=width;
         this.height=height;
         this.parent=parent;
-        this.cellWidth=9;
+        this.cellWidth=10;
         this.createCanvas();
         this.snake=new Snake(this);
         this.foodController = new FoodController(this); 
@@ -18,13 +18,13 @@ export class Board{
         const canvas=document.createElement("canvas");
         const ctx=canvas.getContext("2d");
         this.canvas=ctx;
-        canvas.setAttribute("width",this.width);
-        canvas.setAttribute("height",this.height);
+        canvas.setAttribute("width",this.width * this.cellWidth);
+        canvas.setAttribute("height",this.height * this.cellWidth);
         this.parent.appendChild(canvas);
     }
 
     render=()=>{
-        this.canvas.clearRect(0,0,this.width,this.height);
+        this.canvas.clearRect(0,0,this.width * this.cellWidth,this.height * this.cellWidth);
         this.renderBoard();
         this.snake.render();
         window.requestAnimationFrame(this.render)
@@ -32,7 +32,7 @@ export class Board{
 
     renderBoard=()=>{
         this.canvas.fillStyle="green";
-        this.canvas.fillRect(0,0,this.width,this.height);
+        this.canvas.fillRect(0,0,this.width * this.cellWidth,this.height * this.cellWidth);
         // console.log("DS");
     }
 };
