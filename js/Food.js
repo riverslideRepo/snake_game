c5e99d4cda191dcda6f8ea8b04be98aaf30eba08
Subhendu Hazra <subhenduhazra@Subhendus-MacBook-Air.local>
2021-12-24 21:38:48 +0530

    Restore Point **

--- js/Food.js
+++ js/Food.js
@@ -8,12 +8,17 @@ export class Food{
     }
     render=()=>{
         // console.log(this)
-        this.board.canvas.fillStyle="yellow";
-        // this.board.canvas.beginPath();
-        //  this.board.canvas.fillRect(this.positionX * this.board.cellWidth, this.positionY * this.board.cellWidth,this.cellWidth,this.cellWidth);
-        //  this.board.canvas.closePath();
-        this.board.canvas.fillRect(this.positionX * this.board.cellWidth,this.positionY * this.board.cellWidth,this.board.cellWidth,this.board.cellWidth);
+        this.currentFoodColor="rgb(255,0,0)";
+        this.board.canvas.fillStyle=this.currentFoodColor;
+
+        this.board.canvas.beginPath();
+        let cx = this.positionX * this.board.cellWidth + this.board.cellWidth/2;
+        let cy = this.positionY * this.board.cellWidth + this.board.cellWidth/2;
+        this.board.canvas.arc(cx, cy,this.board.cellWidth/2,0,2*Math.PI);
+        this.board.canvas.closePath();
+        this.board.canvas.fill();
+        //this.board.canvas.fillRect(this.positionX * this.board.cellWidth,this.positionY * this.board.cellWidth,this.board.cellWidth,this.board.cellWidth);
         // console.log(this.positionY * this.board.cellWidth);
-        //  this.board.canvas.fill();
+          
     }
 }
